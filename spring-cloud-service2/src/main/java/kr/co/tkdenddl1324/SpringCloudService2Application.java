c633ac7baba997b4940cc2d73f5cfab57eb2fe0c
tkdenddl1324 <tkdenddl1324@gmail.com>
2025-04-09 16:24:16 +0900

    spring-cloud

---
+++ spring-cloud-service2/src/main/java/kr/co/tkdenddl1324/SpringCloudService2Application.java
@@ -0,0 +1,15 @@
+package kr.co.tkdenddl1324;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class SpringCloudService2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudService2Application.class, args);
+    }
+
+}
---
+++ spring-cloud-service3/src/main/java/kr/co/tkdenddl1324/controller/MainController.java
@@ -0,0 +1,14 @@
+package kr.co.tkdenddl1324.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @GetMapping("/")
+    public String index() {
+        return "service3 index";
+    }
+
+}
